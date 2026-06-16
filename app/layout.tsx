5079cd8bbe0ff61701ae2159d1fd807b6326f4d8
fırat meço <firatmeco9@gmail.com>
2026-06-16 21:54:40 +0300

    fix analytics head placement

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,9 +28,7 @@ export default function RootLayout({
       lang="tr"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        {children}
-
+      <head>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-QYWV9XCQ94"
           strategy="afterInteractive"
@@ -45,6 +43,10 @@ export default function RootLayout({
             gtag('config', 'G-QYWV9XCQ94');
           `}
         </Script>
+      </head>
+
+      <body className="min-h-full flex flex-col">
+        {children}
       </body>
     </html>
   );
