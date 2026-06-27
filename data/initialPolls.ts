c27e16e39b82544f64d9b8d4d1e86060fa7cdc81
fırat meço <firatmeco9@gmail.com>
2026-06-27 14:15:53 +0300

    replace old anonymous usernames

--- data/initialPolls.ts
+++ data/initialPolls.ts
@@ -7,35 +7,26 @@ export const initialPolls: Poll[] = [
     category: "Gündem",
 
     options: [
-      {
-        text: "Evet gerekli",
-        votes: 90,
-      },
-
-      {
-        text: "Hayır kaldırılmalı",
-        votes: 40,
-      },
+      { text: "Evet gerekli", votes: 90 },
+      { text: "Hayır kaldırılmalı", votes: 40 },
     ],
 
     comments: [
       {
         id: 1001,
-        authorName: "AsabiKarınca77",
+        authorName: "TRK265",
         text: "merhaba",
         likes: 0,
       },
-
       {
         id: 1002,
-        authorName: "TurboTost35",
+        authorName: "34ANK99",
         text: "Bence anonimlik dürüstlükten çok kontrolsüzlük getiriyor.",
         likes: 0,
       },
-
       {
         id: 1003,
-        authorName: "ŞüpheliPenguen64",
+        authorName: "Q7TDI",
         text: "Gerçek fikirler genelde anonimken ortaya çıkıyor.",
         likes: 0,
       },
@@ -48,33 +39,21 @@ export const initialPolls: Poll[] = [
     category: "İlişkiler",
 
     options: [
-      {
-        text: "Sürekli yalan söylemesi",
-        votes: 203,
-      },
-
-      {
-        text: "Aşırı kıskançlık",
-        votes: 176,
-      },
-
-      {
-        text: "İlgisizlik",
-        votes: 91,
-      },
+      { text: "Sürekli yalan söylemesi", votes: 203 },
+      { text: "Aşırı kıskançlık", votes: 176 },
+      { text: "İlgisizlik", votes: 91 },
     ],
 
     comments: [
       {
         id: 2001,
-        authorName: "GececiKebap42",
+        authorName: "RSLine",
         text: "Yalan varsa güven bitmiştir.",
         likes: 0,
       },
-
       {
         id: 2002,
-        authorName: "UykusuzMandalina18",
+        authorName: "1453X",
         text: "Kıskançlık zamanla boğuyor.",
         likes: 0,
       },
